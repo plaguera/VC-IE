741bf844a5ec916a4b929db1da81ef4fd0198237
Pedro Lagüera <laguerapedro@gmail.com>
2017-11-15 19:10:32 +0000

    Added new start dialog

--- src/main/ImageProcessor.java
+++ src/main/ImageProcessor.java
@@ -1,7 +1,18 @@
 package main;
 
+import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.UIManager;
 
 import imageFrame.ImageFrame;
@@ -17,12 +28,40 @@ public class ImageProcessor {
 			public void run() {
 				try {
 					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-					ImageUtils.launchFrame(new ImageFrame("/Users/peter/Drive/VC/lena.png"));
+					ImageUtils.launchFrame(new ImageFrame(startWindow()));
+					//ImageUtils.launchFrame(new ImageFrame("/Users/peter/Drive/VC/lena.png"));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
+	
+	public static String startWindow() {
+		ImageIcon icon = new ImageIcon("src/images/picture.png");
+		JPanel panel      = new JPanel();
+		JPanel outerPanel = new JPanel();
+		panel.setLayout( new FlowLayout(FlowLayout.CENTER, 0, 0) );
+		JTextField textField = new JTextField(10);
+		textField.setBorder(BorderFactory.createTitledBorder("Path to Image"));
+		panel.add(textField);
+		JButton button = new JButton("Open...");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				File selectedFile = ImageUtils.openImage();
+				if (selectedFile == null)
+					return;
+				textField.setText(selectedFile.getAbsolutePath());
+			}
+		});
+		panel.add(button);
+		panel.setBackground( textField.getBackground() );
+		panel.setBorder( textField.getBorder() );
+		textField.setBorder(null);
+		outerPanel.add(panel);
 
+        UIManager.put("OptionPane.minimumSize",new Dimension(300, 120));
+        JOptionPane.showMessageDialog(null, outerPanel, "Open Image", JOptionPane.PLAIN_MESSAGE, icon);
+        return textField.getText();
+	}
 }
